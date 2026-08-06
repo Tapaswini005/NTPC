a1027cc147e75f5e4cfe29f5177bddc6fb436c28
Tapaswini Shaw <shawtapaswini@gmail.com>
2026-08-06 19:35:41 +0530

    Updated files

---
+++ code.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score
