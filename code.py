0f52095c0bb0dc7db15cfbaa6cd9a30f409083ea
Tapaswini005 <shawtapaswini2005@gmail.com>
2026-08-07 23:40:02 +0530

    Update file

--- code.py
+++ code.py
@@ -1,9 +1,4 @@
-import streamlit as st
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import accuracy_score
+
 
 st.set_page_config(page_title="Predictive Maintenance System")
 
